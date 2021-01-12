ad4ecf7609701a90e7b75e0d6080db60a8051a2a
Rohan Gupta <rohangupta2036@gmail.com>
2021-01-12 18:11:13 -0500

    condensed to one generalized discrete empirical conditional distribution function

--- conditionals.py
+++ conditionals.py
@@ -5,31 +5,31 @@ import matplotlib as plt
 # DEFCONSL
 # FCOUNSEL
 
-def discrete_mean_times(df,group,time_col):
-    """
-    Computes conditional distribution over the group using one time column
-    """
-    temp = df.dropna(subset = [group])
-    grouped = temp.groupby(group)
-    return grouped[time_col].mean()
+# def discrete_mean_times(df,group,time_col):
+#     """
+#     Computes conditional distribution over the group using one time column
+#     """
+#     temp = df.dropna(subset = [group])
+#     grouped = temp.groupby(group)
+#     return grouped[time_col].mean()
 
 
-def discrete_summed_mean_times(df,group,time_cols):
+def discrete_summed_mean_times(df,groups,time_cols):
     """
-    Computes conditional distribution over the group summing over all of the
-    columns in time_cols
+    Computes conditional distribution over the groups summing over all of the
+    columns in time_cols. both groups and time_cols should be lists.
     """
-    temp = df.dropna(subset = [group])
+    temp = df.dropna(subset = groups)
     temp['total_time'] = temp[time_cols].sum(axis=1)
-    grouped = temp.groupby(group)
+    grouped = temp.groupby(groups)
     return grouped['total_time'].mean()
 
 
 if __name__ == "__main__":
     df = pd.read_csv("justfair_dataset.csv")
-    summed_means = discrete_summed_mean_times(df,"judge",['INT1','INT2','INT3'])
+    summed_means = discrete_summed_mean_times(df,["judge","CRIMHIST"],['INT1','INT2','INT3'])
     summed_means.plot.hist(bins=20)
-    means = discrete_mean_times(df,"judge","INT2") # we should decide on which columns specifically we want to use
+    means = discrete_summed_mean_times(df,["judge","CRIMHIST"],["INT2"]) # we should decide on which columns specifically we want to use
     means.plot.hist()
 
     plt.pyplot.show()
