2b67027e68bab7e5b22ef6bb8ed4dffdf91d5938
Rohan Gupta <rohangupta2036@gmail.com>
2021-01-12 00:01:33 -0500

    added name == main check to judges.py

--- judges.py
+++ judges.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("justfair_dataset.csv")
-
 def judge_mean_times(df,judge_col,time_col):
     """
     Calculates the mean case time for each judge (as identified by judge_col)
@@ -11,6 +9,8 @@ def judge_mean_times(df,judge_col,time_col):
     group = temp.groupby(judge_col)
     return group[time_col].mean()
 
-means = judge_mean_times(df,"judge","INT2") # we should decide on which columns specifically we want to use
-print(means)
-# means.to_csv("judge_means.csv") # don't want to lose the histogram by overwriting the file
+if __name__ == "__main__":
+    df = pd.read_csv("justfair_dataset.csv")
+    means = judge_mean_times(df,"judge","INT2") # we should decide on which columns specifically we want to use
+    print(means)
+    # means.to_csv("judge_means.csv") # don't want to lose the histogram by overwriting the file
